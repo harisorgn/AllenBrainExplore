cdf1b899b07d99709910fd7c9f20efcf9a4df1dc
Haris Orgn <organtzh@gmail.com>
2026-01-26 18:50:27 +0200

    use `string` for String conversion

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,5 @@
 function tight_vector_pyconvert(pyv)
-    return pyconvert(String, pyv.dtype.name) == "object" ? pyconvert(Vector{String}, pyv) : pyconvert(Vector, pyv)
+    return pyconvert(String, pyv.dtype.name) == "object" ? string.(collect(pyv)) : pyconvert(Vector, pyv)
 end
 
 to_dataframe(pd) = DataFrame([pyconvert(String, col) => tight_vector_pyconvert(pd[col]) for col in pd.columns])
