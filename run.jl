a8250a6b721f2857b4d5fd1cac0662d73749c5d5
Haris Orgn <organtzh@gmail.com>
2026-02-06 17:13:54 +0200

    update run script to write receptor genes with descriptions

--- run.jl
+++ run.jl
@@ -5,18 +5,20 @@ using CSV
 
 datasets = ["MERFISH-C57BL6J-638850", "Zhuang-ABCA-1", "Zhuang-ABCA-2", "Zhuang-ABCA-3", "Zhuang-ABCA-4", "MERFISH-C57BL6J-638850-imputed"]
 
-df = get_expression_matrix(datasets[1], "VTA")
+region = "LH"
 
+df = get_expression_matrix(datasets[1], region)
 GC.gc()
 
-G = Matrix(df[:, Not(:obs)])
+dfg = get_gene_description(datasets[1], region)
+filter!(r -> occursin("receptor", r.name), dfg)
+
+G = Matrix(df[:, lowercase.(dfg.gene_symbol)])
 
 Gm = vec(mean(G; dims=1))
 
 idxs = sortperm(Gm; rev=true)
 
-genes = names(df[:, Not(:obs)])
+dff = DataFrame([:gene => dfg.gene_symbol[idxs], :description => dfg.name[idxs], :expression_LH => Gm[idxs]])
 
-dfg = DataFrame([:gene => genes[idxs], :expression_VTA => Gm[idxs]])
-
-CSV.write("genes_VTA.csv", dfg)
+CSV.write("receptor_genes_$(region).csv", dff)
