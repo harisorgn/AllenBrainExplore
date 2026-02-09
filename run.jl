928f6417761614a1f96b6ccb0eb1e3f1a089da9d
Haris Orgn <organtzh@gmail.com>
2026-02-09 11:14:10 +0200

    add standard deviation in gene csvs and sort by it

--- run.jl
+++ run.jl
@@ -5,7 +5,7 @@ using CSV
 
 datasets = ["MERFISH-C57BL6J-638850", "Zhuang-ABCA-1", "Zhuang-ABCA-2", "Zhuang-ABCA-3", "Zhuang-ABCA-4", "MERFISH-C57BL6J-638850-imputed"]
 
-region = "LH"
+region = "ACB"
 
 df = get_expression_matrix(datasets[1], region)
 GC.gc()
@@ -16,9 +16,10 @@ filter!(r -> occursin("receptor", r.name), dfg)
 G = Matrix(df[:, lowercase.(dfg.gene_symbol)])
 
 Gm = vec(mean(G; dims=1))
+Gstd = vec(std(G; dims=1))
 
-idxs = sortperm(Gm; rev=true)
+idxs = sortperm(Gstd; rev=true)
 
-dff = DataFrame([:gene => dfg.gene_symbol[idxs], :description => dfg.name[idxs], :expression_LH => Gm[idxs]])
+dff = DataFrame([:gene => dfg.gene_symbol[idxs], :description => dfg.name[idxs], :expression_ACB => Gm[idxs], :std_expression_ACB => Gstd[idxs]])
 
 CSV.write("receptor_genes_$(region).csv", dff)
